20ea7be766aa6a114ed74e84d1c58d6815a2a378
Justin <justleland@gmail.com>
2019-06-03 11:05:43 -0400

    Passed MultiplesDeleter Tests

--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import java.util.ArrayList;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -10,7 +12,16 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with evens removed
      */
     public Integer[] deleteEvens(Integer[] ints) {
-        return null;
+
+        ArrayList<Integer> oddsList = new ArrayList<Integer>();
+
+        for(Integer num : ints){
+            if(num % 2 != 0) oddsList.add(num);
+        }
+
+        Integer[] odds = listToArray(oddsList);
+
+        return odds;
     }
 
     /**
@@ -19,7 +30,16 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with odds removed
      */
     public Integer[] deleteOdds(Integer[] ints) {
-        return null;
+
+        ArrayList<Integer> evensList = new ArrayList<Integer>();
+
+        for(Integer num : ints) {
+            if(num % 2 == 0) evensList.add(num);
+        }
+
+        Integer[] evens = listToArray(evensList);
+
+        return evens;
     }
 
     /**
@@ -28,7 +48,16 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by 3 removed
      */
     public Integer[] deleteMultiplesOf3(Integer[] ints) {
-        return null;
+
+        ArrayList<Integer> notMultiplesof3List = new ArrayList<Integer>();
+
+        for(Integer num : ints) {
+            if(num % 3 != 0) notMultiplesof3List.add(num);
+        }
+
+        Integer[] notMultiplesof3 = listToArray(notMultiplesof3List);
+
+        return notMultiplesof3;
     }
 
     /**
@@ -38,6 +67,23 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by `multiple` removed
      */
     public Integer[] deleteMultiplesOfN(Integer[] ints, int multiple) {
-        return null;
+
+        ArrayList<Integer> notMulitplesOfNList = new ArrayList<Integer>();
+
+        for(Integer num : ints) {
+            if(num % multiple != 0) notMulitplesOfNList.add(num);
+        }
+
+        Integer[] notMultiplesOfN = listToArray(notMulitplesOfNList);
+
+        return notMultiplesOfN;
+    }
+
+    private Integer[] listToArray(ArrayList<Integer> list) {
+        Integer[] convert = new Integer[list.size()];
+        for(int i = 0; i < convert.length; i++) {
+            convert[i] = list.get(i);
+        }
+        return convert;
     }
 }
