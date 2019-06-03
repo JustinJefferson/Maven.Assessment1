8ee145f1fc720146433d691000cbaa07462c78eb
Justin <justleland@gmail.com>
2019-06-03 10:03:44 -0400

    Passed IntegerUtils Tests

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -11,7 +11,14 @@ public class IntegerUtils {
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+
+        Integer sum = 0;
+
+        for(int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        return sum;
     }
 
     /**
@@ -19,7 +26,14 @@ public class IntegerUtils {
      * @return the product of all integers between 0 and not including `n`
      */
     public static Integer getProductOfN(Integer n) {
-        return null;
+
+        Integer product = 1;
+
+        for(int i = 2; i <= n; i++) {
+            product *= i;
+        }
+
+        return product;
     }
 
     /**
@@ -27,6 +41,11 @@ public class IntegerUtils {
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+
+        StringBuilder value = new StringBuilder(val.toString());
+        value.reverse();
+        Integer reversed = Integer.valueOf(value.toString());
+
+        return reversed;
     }
 }
