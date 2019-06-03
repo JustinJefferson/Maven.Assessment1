070658ef0ee6f15df0a5dd4751f0e3e8c6398742
Justin <justleland@gmail.com>
2019-06-03 12:36:39 -0400

    Passed AnimalsPetsOwners Test

--- src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
@@ -4,17 +4,24 @@ package com.zipcodewilmington.assessment1.part3;
  * Created by leon on 2/16/18.
  */
 public abstract class Pet implements Animal {
+    public String name;
+    public int age;
+    private PetOwner petOwner;
     /**
      * nullary constructor
      * by default, pet has age of 0; name of "";
      */
     public Pet() {
+        name = "";
+        age = 0;
     }
 
     /**
      * @param name name of this pet
      */
     public Pet(String name) {
+        this.name = name;
+        age = 0;
     }
 
 
@@ -22,6 +29,8 @@ public abstract class Pet implements Animal {
      * @param age age of this pet
      */
     public Pet(int age) {
+        this.age = age;
+        name = "";
     }
 
     /**
@@ -29,20 +38,22 @@ public abstract class Pet implements Animal {
      * @param age age of this pet
      */
     public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     /**
      * @return name of this pet
      */
     public String getName() {
-        return null;
+        return name;
     }
 
     /**
      * @return age of this pet
      */
     public Integer getAge() {
-        return null;
+        return age;
     }
 
     /**
@@ -50,12 +61,13 @@ public abstract class Pet implements Animal {
      * ensure this instance of `Pet` is added to the owner's composite `pets` list
      */
     public void setOwner(PetOwner newPetOwner) {
+        petOwner = newPetOwner;
     }
 
     /**
      * @return PetOwner object whose composite `pets` collection contains this Pet instance
      */
     public PetOwner getOwner() {
-        return null;
+        return petOwner;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
@@ -1,26 +1,81 @@
 package com.zipcodewilmington.assessment1.part3;
 
+
 /**
  * Created by leon on 2/16/18.
  */
 public class PetOwner {
+    private String name;
+    private Pet[] pets;
     /**
      * @param name name of the owner of the Pet
      * @param pets array of Pet object
      */
     public PetOwner(String name, Pet... pets) {
+        this.name = name;
+        if(pets != null) {
+            this.pets = pets;
+            for(Pet pet : this.pets) {
+                pet.setOwner(this);
+            }
+        }
+        else{
+            this.pets = new Pet[1];
+        }
     }
 
     /**
      * @param pet pet to be added to the composite collection of Pets
      */
     public void addPet(Pet pet) {
+
+        Boolean spaceAvailible = false;
+
+        for(int i = 0; i < pets.length; i++) {
+            if(pets[i] == null) {
+                pets[i] = pet;
+                spaceAvailible = true;
+            }
+
+        }
+
+
+
+        if(!spaceAvailible) {
+
+            Pet[] newPets = new Pet[pets.length + 1];
+            Integer index = 0;
+
+            for (Pet thisPet : pets) {
+                newPets[index] = thisPet;
+                index++;
+            }
+
+            newPets[newPets.length - 1] = pet;
+
+            pets = newPets;
+        }
+
+
+
     }
 
     /**
      * @param pet pet to be removed from the composite collection Pets
      */
     public void removePet(Pet pet) {
+
+        Pet[] newPets = new Pet[pets.length];
+        Integer index = 0;
+
+        for(Pet thisPet : pets) {
+            if(!thisPet.equals(pet)) {
+                newPets[index] = thisPet;
+                index++;
+            }
+        }
+
+        pets = newPets;
 
     }
 
@@ -29,14 +84,31 @@ public class PetOwner {
      * @return true if I own this pet
      */
     public Boolean isOwnerOf(Pet pet) {
-        return null;
+
+        Boolean owner = false;
+
+        for(Pet thisPet : pets) {
+            if(thisPet.equals(pet)) {
+                owner = true;
+                break;
+            }
+        }
+        return owner;
     }
 
     /**
      * @return the age of the Pet object whose age field is the lowest amongst all Pets in this class
      */
     public Integer getYoungetPetAge() {
-        return null;
+
+        Integer age = null;
+
+        for(Pet pet : pets) {
+            if((age == null) || (pet.getAge() < age)) {
+                age = pet.getAge();
+            }
+        }
+        return age;
     }
 
 
@@ -46,7 +118,14 @@ public class PetOwner {
      * @return the age of the Pet object whose age field is the highest amongst all Pets in this class
      */
     public Integer getOldestPetAge() {
-        return null;
+        Integer age = null;
+
+        for(Pet pet : pets) {
+            if((age == null) || (pet.getAge() > age)) {
+                age = pet.getAge();
+            }
+        }
+        return age;
     }
 
 
@@ -54,27 +133,36 @@ public class PetOwner {
      * @return the sum of ages of Pet objects stored in this class divided by the number of Pet object
      */
     public Float getAveragePetAge() {
-        return null;
+
+        Float average = (float) 0;
+
+        for(Pet pet : pets) {
+            average += pet.getAge();
+        }
+
+        average /= pets.length;
+
+        return average;
     }
 
     /**
      * @return the number of Pet objects stored in this class
      */
     public Integer getNumberOfPets() {
-        return null;
+        return pets.length;
     }
 
     /**
      * @return the name property of the Pet
      */
     public String getName() {
-        return null;
+        return name;
     }
 
     /**
      * @return array representation of animals owned by this PetOwner
      */
     public Pet[] getPets() {
-        return null;
+        return pets;
     }
 }
