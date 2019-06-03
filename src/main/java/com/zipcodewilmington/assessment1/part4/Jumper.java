b6b6de453dc1ba104328dd497c45c24aefa46156
Justin <justleland@gmail.com>
2019-06-03 13:33:18 -0400

    Passed Jump the Flag Tests

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,6 +6,23 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
-        return -1;
+
+        Integer flagHeight = k;
+        Integer jumpHeight = j;
+        Integer currentHeight = 0;
+        Integer jumps = 0;
+
+        while(currentHeight < flagHeight) {
+            if(currentHeight + jumpHeight <= flagHeight) {
+                currentHeight += jumpHeight;
+            }
+            else {
+                currentHeight++;
+            }
+
+            jumps++;
+        }
+
+        return jumps;
     }
 }
